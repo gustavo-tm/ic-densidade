ca59e09354721749e5f7bd91df2851e4feb289e0
gustavo-tm <gtheilm@gmail.com>
2024-05-19 15:41:08 -0300

    entrega tcc1

--- src.R
+++ src.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(sf)
 
+distrito <- read_sf("dados/distrito/SIRGAS_SHP_distrito.shp") %>% st_set_crs("epsg:31983")
+
 IPTU <- read.csv("dados/IPTU/IPTU_2024.csv", sep=";", encoding = "latin1") %>% 
   as_tibble() %>% 
   select(sql = "NUMERO.DO.CONTRIBUINTE", 
@@ -16,12 +18,11 @@ IPTU <- read.csv("dados/IPTU/IPTU_2024.csv", sep=";", encoding = "latin1") %>%
 
 for (file in list.files(path="dados/lotes/zip", full.names = FALSE) %>% 
      str_remove("\\.zip")){
-  
   unzip(paste("dados/lotes/zip/", file, ".zip", sep = ""), paste(file, "/", file, ".shp", sep = ""), exdir = "dados/lotes/unzip")
   unzip(paste("dados/lotes/zip/", file, ".zip", sep = ""), paste(file, "/", file, ".dbf", sep = ""), exdir = "dados/lotes/unzip")
   unzip(paste("dados/lotes/zip/", file, ".zip", sep = ""), paste(file, "/", file, ".shx", sep = ""), exdir = "dados/lotes/unzip")
-  
-}
+} %>% 
+remove(file)
 
 lotes <- list.files(path="dados/lotes/unzip", full.names = FALSE) %>% 
   paste("dados/lotes/unzip/", ., "/", ., ".shp", sep = "") %>% 
@@ -33,18 +34,49 @@ lotes <- list.files(path="dados/lotes/unzip", full.names = FALSE) %>%
 IPTU.lotes <- IPTU %>% 
   left_join(lotes)
 
-censo <- read_sf("dados/censo/SP_Malha_Preliminar_2022.shp")
+remove(IPTU)
+remove(lotes)
 
-gg <- censo %>% 
+censo <- read_sf("dados/censo/SP_Malha_Preliminar_2022.shp") %>% 
   filter(CD_MUN == "3550308") %>% 
-  mutate(densidade = v0001/AREA_KM2,
-         decil = ntile(densidade, 10)) %>% 
-  ggplot() +
-  geom_sf(aes(fill = factor(decil)), color = NA) +
+  st_transform("epsg:31983")
+
+gg <- ggplot() +
+  geom_sf(data = censo %>% 
+            mutate(densidade = v0001/AREA_KM2,
+                   decil = ntile(densidade, 10)),
+          aes(fill = factor(decil)), color = NA) +
   scale_fill_viridis_d() +
   labs(fill = "Decil de densidade") +
   theme_void()
 
-ggsave("mapa.png", gg, width = 20, height = 20, dpi = 400)
+ggsave("tex/imagens/mapa.png", gg, width = 10, height = 15, dpi = 300)
 
 
+
+censo %>% 
+  filter(CD_MUN == "3550308") %>% 
+  st_drop_geometry() %>% 
+  summarize(pop = sum(v0001),
+            dom = sum(v0002),
+            dom_ocupados = sum(v0007))
+
+
+gg <- ggplot() +
+  geom_sf(data = censo %>%
+            filter(CD_MUN == "3550308") %>%
+            mutate(densidade = v0001/AREA_KM2,
+                   decil = ntile(densidade, 10)) %>% 
+            st_crop(distrito %>% filter(ds_nome == "LAPA")),
+          aes(fill = factor(decil)), color = "white", lwd = 1.5) +
+  geom_sf(data = IPTU.lotes %>% 
+            select(geometry) %>% 
+            st_as_sf() %>% 
+            st_crop(distrito %>% filter(ds_nome == "LAPA")), 
+          aes(geometry = geometry), fill = "red", color = "red", alpha = .25) +
+  scale_fill_viridis_d() +
+  labs(fill = "Decil de densidade") +
+  theme(legend.position = "none") +
+  theme_void()
+
+ggsave("tex/imagens/mapa-lotes.png", gg, width = 20, height = 20, dpi = 300)
